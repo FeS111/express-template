45610fd819fcdb6ab6795f39d1733469eb222c8d
felix <felix.schmid@siriusonline.de>
2021-04-23 13:52:54 +0200

    added middlewars

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response, Application } from 'express';
 import * as bodyParser from 'body-parser';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 8080;
 
 const app: Application = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('express-template');
